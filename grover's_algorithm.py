43cfc7b1e67855b63bcfdcfbb292050cb1ef5f08
M N Yugendran <158173054+user-230087@users.noreply.github.com>
2024-10-07 00:33:42 +0530

    Add files via upload

---
+++ grover's_algorithm.py
@@ -0,0 +1,36 @@
+# Built-in modules
+import math
+
+# Imports from Qiskit
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import GroverOperator, MCMT, ZGate
+from qiskit.visualization import plot_distribution
+
+# Imports from Qiskit Runtime
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+
+def grover_oracle(marked_states):
+    if not isinstance(marked_states, list):
+        marked_states = [marked_states]
+    # Compute the number of qubits in circuit
+    num_qubits = len(marked_states[0])
+
+    qc = QuantumCircuit(num_qubits)
+    # Mark each target state in the input list
+    for target in marked_states:
+        # Flip target bit-string to match Qiskit bit-ordering
+        rev_target = target[::-1]
+        # Find the indices of all the '0' elements in bit-string
+        zero_inds = [ind for ind in range(num_qubits) if rev_target.startswith("0", ind)]
+        # Add a multi-controlled Z-gate with pre- and post-applied X-gates (open-controls)
+        # where the target bit-string has a '0' entry
+        qc.x(zero_inds)
+        qc.compose(MCMT(ZGate(), num_qubits - 1, 1), inplace=True)
+        qc.x(zero_inds)
+    return qc
+
+marked_states = ["011", "100"]
+
+oracle = grover_oracle(marked_states)
+oracle.draw(output="mpl", style="iqp")
